792bd9c5a094306d9437e40735468e179d505705
Will Prendergast <prendygast@hotmail.com>
2020-02-11 00:00:11 +0000

    moving onto Products views and urls

---
+++ products/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Product
+
+class ProductTests(TestCase):
+    """
+    Here we will deine the tests 
+    that we'll run against our Product models
+    """
+    def test_str(self):
+        test_name = Product(name='A Product')
+        self.assertEqual(str(test_name), 'A Product')
